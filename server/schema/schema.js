40466d0bfa9c61c4c358dba663f758b2b1711cf9
Kumaar-Balbir <prasadbalbir1056@gmail.com>
2024-04-06 09:20:02 +0530

    mutation: delete client

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -95,6 +95,16 @@ const mutation = new GraphQLObjectType({
         return client.save();
       },
     },
+    // Delete a client
+    deleteClient: {
+      type: ClientType,
+      args: {
+        id: { type: GraphQLNonNull(GraphQLID) },
+      },
+      resolve(parent, args) {
+        return Client.findByIdAndDelete(args.id);
+      },
+    },
   },
 });
 module.exports = new GraphQLSchema({

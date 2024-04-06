d312e58a01e55016e19f306d4d6b0ee400936eb1
Kumaar-Balbir <prasadbalbir1056@gmail.com>
2024-04-06 12:47:15 +0530

    fix: apollo client warning - cache data may be lost

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,31 @@ import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 import Clients from "./components/Clients";
 
+/* This code snippet is creating an instance of `InMemoryCache` from the Apollo Client library. The
+`InMemoryCache` is used to store and manage the client-side cache for GraphQL data in your
+application. */
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        clients: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+        projects: {
+          merge(existing, incoming) {
+            return incoming;
+          },
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: "http://localhost:5000/graphql",
-  cache: new InMemoryCache(),
+  cache: cache,
 });
 
 function App() {

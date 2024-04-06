96b87d25bc2412ff97ce9cf9c1c5b5a104c2e9e5
Kumaar-Balbir <prasadbalbir1056@gmail.com>
2024-04-06 12:23:38 +0530

    update: spinner instead of loading

--- client/src/components/Clients.jsx
+++ client/src/components/Clients.jsx
@@ -1,10 +1,11 @@
 // role of gql: to get data from server
 import { gql, useQuery } from "@apollo/client";
 import ClientRow from "./ClientRow";
+import Spinner from "./Spinner";
 import { GET_CLIENTS } from "../queries/clientQueries";
 export default function Clients() {
   const { loading, error, data } = useQuery(GET_CLIENTS);
-  if (loading) return <p> loading...</p>;
+  if (loading) return <Spinner />;
   if (error) return <p>Something went wrong</p>;
   return (
     <>
